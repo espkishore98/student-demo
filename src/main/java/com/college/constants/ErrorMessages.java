735971746d84d868ce5cc8c52071f1c2b36fa862
espkishore <espkishore@gmail.com>
2020-08-04 19:26:17 +0530

    Added Validations

--- src/main/java/com/college/constants/ErrorMessages.java
+++ src/main/java/com/college/constants/ErrorMessages.java
@@ -3,5 +3,13 @@ package com.college.constants;
 public interface ErrorMessages {
 
 	public final String REGISTRATION_FAILED = "User Registration Failed.";
+	public final String PROVIDE_FIRST_NAME = "Please Enter Your First Name";
+	public final String PROVIDE_LAST_NAME = "Please Enter Your Last Name";
+	public final String PROVIDE_EMAIL_ID = "Please Provide Your Email Id";
+	public final String PROVIDE_PASSWORD = "Please Enter Your Password";
+	public final String PROVIDE_DEPARTMENT_NAME ="Please Enter Your Department name ";
+	public final String CHECK_EMAIL_FORMAT = "Please Check Email Format";
+	public final String PROVIDE_USER_ID = "Please Provide UserId";
+	public final String PROVIDE_USER_TYPE = "Please Provide User Type";
 
 }
